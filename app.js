40222b82ea964f143daca2e9675796a45133a26e
Amir <maralani@kpp.ir>
2020-10-20 18:02:22 +0330

    Fix banner + Add some projects + Contact Page

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/index', indexRouter);
 app.use('/resume', resumeRouter);
 app.use('/projects', projectsRouter);
 app.use('/blog', blogRouter);
