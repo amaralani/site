0945ff0070f955d26a3b95bce50206bde5cfb7c0
Amir <maralani@kpp.ir>
2020-11-24 15:57:03 +0330

    +Blog

--- routes/blog.js
+++ routes/blog.js
@@ -1,8 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const https = require('https')
 
-router.get('/', function(req, res, next) {
-  res.render('blog', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  const options = {
+    hostname: 'dev.to',
+    path: '/api/articles/me/published',
+    method: 'GET',
+    headers: { "api-key": "jUZKis2Mhozb2sxJNE2vHSk4" }
+  }
+  let articlesString = '';
+  const request = https.request(options, response => {
+    response.on('data', data => {
+      articlesString += data;
+    })
+
+    response.on('error', error => {
+      console.error(error)
+    })
+  
+    response.on('end', error => {
+      let articles = JSON.parse(articlesString);
+      res.render('blog', { title: 'Express' , articles : articles.filter(article => article.type_of ==='article') });
+    })
+  })
+
+  request.end()
+  
 });
 
 module.exports = router;
